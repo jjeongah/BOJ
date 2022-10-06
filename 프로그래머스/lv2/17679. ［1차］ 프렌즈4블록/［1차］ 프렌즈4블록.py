682e334176ccee2de9b432ce069bababb091ab26
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2022-10-06 10:03:18 +0900

    Update ［1차］ 프렌즈4블록.py

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,13 +1,16 @@
 def solution(m, n, board):
     # input: m은 행의 개수, n의 열의 개수
-    # outpu: 몇 개 블록이 지워질지
+    # output: 몇 개 블록이 지워질지
     answer = 0 # 총 사라진 블록 수 
     
     # board를 2차원 list로
     b = [[j for j in range(n)] for i in range(m)]
     for i in range(m):
         for j in range(n):
-            b[i][j] = board[i][j]
+            b[i][j] = board[i][j] 
+            # tip: for i in range(m): board[i] = list(board[i])
+                  # b = [list(x) for x in board]
+                
     # issue: 어떻게 없어진 애 처리?
     # 2*2 찾고 0으로 -> 0인 애들은 무시하고 2*2를 찾게
     while True: # 알아서 종료될 때까지 돌림
@@ -15,9 +18,7 @@ def solution(m, n, board):
         pop = [] # 이번에 터져야 할 블록들 모음
         for i in range(m-1):
             for j in range(n-1):
-                if b[i][j] == '0':
-                    continue
-                if b[i][j] == b[i][j+1] and b[i][j] == b[i+1][j] and b[i][j] == b[i+1][j+1]:
+                if b[i][j] == b[i][j+1] == b[i+1][j] == b[i+1][j+1] != '0': # tip
                     pop.append((i,j))
                     pop.append((i+1,j))
                     pop.append((i,j+1))
@@ -28,14 +29,11 @@ def solution(m, n, board):
         for p in pop:
             b[p[0]][p[1]] = '0'
         
-        # issue: 블록들 내리기(???)
-        for p in reversed(pop): 
-            x = p[0]-1
-            y = p[0]
-            while x >= 0: #터진 자리 위에 있는 블록 내리기
-                if b[y][p[1]] == '0' and b[x][p[1]] != '0':
-                    b[y][p[1]] = b[x][p[1]]
-                    b[x][p[1]] = '0'
-                    y -= 1
-                x -= 1
+        # issue: 블록들 내리기-> 블록이 아래로 떨어져 빈 공간을 채우는 부분 
+        for _ in range(m):
+            for i in range(m-1):
+                for j in range(n):
+                    if b[i+1][j] == '0': #아래가 비어있다면
+                        b[i+1][j] = b[i][j]  
+                        b[i][j] = '0'
     return answer
