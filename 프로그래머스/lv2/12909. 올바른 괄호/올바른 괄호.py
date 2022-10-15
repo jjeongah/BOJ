09238e2aa86fdf914dc8ae676220cf286f682d8d
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2022-10-15 20:48:02 +0900

    Update 올바른 괄호.py

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -6,6 +6,7 @@ def solution(s):
         elif s == ")":
             try:
                 arr.pop()
+                #pop(0)가 더 오래 걸림
             except:
                 return False
     if arr == []:
