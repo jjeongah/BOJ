6afdac1ea6542ad2faf5d9ab341cd29eeb7ad7b1
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2022-10-17 22:39:31 +0900

    [level 2] Title: 후보키, Time: 0.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,40 +1,23 @@
-# python의 자료형을 적재적소에 쓰는 게 어려웠던 문제
+from itertools import combinations
+from functools import partial
 
-from itertools import combinations 
+
 def solution(relation):
-    '''
-        2차원 배열 relation [속성][튜플]
-        후보 키의 개수를 return
-        
-        idea: combinations으로 전체 경우의 수 구하고 아래 기준으로 거른다
-              1. 유일성 -> set에 넣었을 때랑 개수가 동일
-              2. 최소성 -> 부분집합이 아니어야 함
-        issue: 두 개 이상의 속성을 조합했을 때 경우의 수 어떻게 구할까
-    '''
-    answer = 0 # 후보키의 개수
-    row, col = len(relation), len(relation[0]) #2차원 배열의 행, 렬 구하기
-    candidates = [] # 원소값이 아닌 키의 idx 경우의 수
-    for i in range(1, col +1): # col+1 개의 키 중 1개, 2개, ... col+1개를 뽑는 경우
-        candidates.extend(combinations(range(col), i))
-        # append는 리스트 끝에 1개를 그대로 넣음/ extend는 같은 배열로 추가
-    #print(candidates)
-    
-    # idx candiated로 실제 값들 비교
-    unique_can = []
-    for cand in candidates:
-        all_can = [tuple(row[col] for col in cand) for row in relation]
-        # tuple의 역할 모르겠음
-        '''
-        for row in relation: # 하나의 row
-            for col in cand: 
-                all_can.append(row[col])
-        '''
-        if len(all_can) == len(set(all_can)): # 1. 유일성
-            flag = True
-            for x in unique_can: # 2. 최소성
-                if set(x).issubset(set(cand)):
-                    flag = False
-            if flag: 
-                unique_can.append(cand)
-    print(unique_can)
-    return len(unique_can)
+    candidate_key = set()
+    # 후보키가 될수 있는 조합 찾기
+    # r: nCr 에서의 r
+    col = len(relation[0])
+    for r in range(1, col + 1):
+        for case in combinations(range(col), r):
+            issuperset = partial(set.issuperset, set(case))
+            # (1) 최소성
+            # 이미 확인된 후보키 중, 부분집합이 있나 확인->후보키중 하나라도 부분집합이 있으면, continue.
+            if any(map(issuperset, candidate_key)):
+                continue
+
+            # (2) 유일성
+            case_set = set([tuple(a[idx] for idx in case) for a in relation])
+            if len(relation) == len(case_set):
+                candidate_key.add(case)
+                
+    return len(candidate_key)
