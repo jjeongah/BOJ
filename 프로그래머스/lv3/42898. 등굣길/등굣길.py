3b244e8909ed5507f68b0bb816c6197a8415ebab
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2022-10-28 09:57:01 +0900

    [level 3] Title: 등굣길, Time: 3.33 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -13,17 +13,17 @@ def solution(m, n, puddles):
     dp[i][j] : (i,j)까지 갈 수 있는 경우의 수
     dp[i][j] = dp[i-1][j] + dp[i][j-1] (왼, 오 두 방향만 갈 수 있으므로)
     '''
-    puddles = [[b,a] for [a,b] in puddles] 
-    dp = [[0]*(m+1) for i in range(n+1)] # dp 초기화
-    dp[1][1] = 1 # 집(시작)의 위치
+    puddles = [[b-1,a-1] for [a,b] in puddles] 
+    dp = [[0]*(m) for i in range(n)] # dp 초기화
+    dp[0][0] = 1 # 집(시작)의 위치
     
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            if n == 1 and m == 1:
+    for i in range(n):
+        for j in range(m):
+            if n == 0 and m == 0:
                 continue
             if [i,j] in puddles:
                 dp[i][j] = 0
             else:
                 dp[i][j] += (dp[i-1][j] + dp[i][j-1])
     print(dp)             
-    return dp[n][m]  % 1000000007
+    return dp[n-1][m-1]  % 1000000007
