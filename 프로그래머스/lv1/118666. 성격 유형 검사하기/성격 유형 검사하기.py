40b78b21c5e6be0063badf3b8e3d00bd3834f1fe
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2023-01-05 09:04:20 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,40 +1,18 @@
-'''
-더 높은 점수를 받은 게 성격 유형(지표)
-점수가 같으면 사전 순으로 빠른 거
-
-survey: 질문이 판단하는 지표 (XY. 비동의시 X, 동의시 Y)
-choices: 선택
-[1- 매우 비동의(+3), 2-비동의(+2), 3-약간 비동의(+1), 4, 5-약간 동의(+1), 6-동의(+2), 7-매우 동의(+3)]
-검사자의 결과를 지표 번호 순서대로 입력
-'''
 def solution(survey, choices):
     answer = ''
-    type = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
-    score = {x:0 for x in type}
-    #print(score)
-    
+    RTCFJMAN = [0,0,0,0,0,0,0,0]
+    str = "RTCFJMAN"
     for i in range(len(survey)):
-        X, Y = survey[i][0], survey[i][1]
-        #print(X, Y)
-        if choices[i] < 4: # 비동의 (X)
-            score[X] += 4-choices[i]
-        else: # 동의(Y)
-            score[Y] += choices[i]-4 
-            
-    if score['R'] < score['T']:
-        answer += 'T'
-    else:
-        answer +='R'
-    if score['C'] < score['F']:
-        answer += 'F'
-    else:
-        answer +='C'
-    if score['J'] < score['M']:
-        answer +='M'
-    else:
-        answer +='J'
-    if score['A'] < score['N']:
-        answer +='N'
-    else:
-        answer +='A'
+        RTCFJMAN[str.index(survey[i][0])] += 4- choices[i]
+    #print(RTCFJMAN) # 테케1에서 [0, 3, 0, -1, -2, 0, 1, 1]
+    
+    if(RTCFJMAN[0]>=RTCFJMAN[1]): answer+= "R"
+    else: answer+="T"
+    if(RTCFJMAN[2]>=RTCFJMAN[3]): answer+= "C"
+    else: answer+="F"
+    if(RTCFJMAN[4]>=RTCFJMAN[5]): answer+= "J"
+    else: answer+="M"
+    if(RTCFJMAN[6]>=RTCFJMAN[7]): answer+= "A"
+    else: answer+="N"
+
     return answer
