67d6dc00fc3b42ad00af8020b502e2f232decdb8
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2023-03-01 17:19:34 +0900

    [unrated] Title: 미로 탈출, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -8,56 +8,13 @@ bfs
 idea: *레버 어떻게 처리? => 레버로 가는 로직 + 레버에서 도착점으로
 *visit 어떻게 체크? => 
 *cnt 어떻게 계산? => 별도의 리스트 사용
+'''
 
 from collections import deque
 def solution(maps):
-    arr = []
-    for map in maps:
-        arr.append(list(map))
-    row = len(arr)
-    col = len(arr[0])
+    answer = 0
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    # 1. 어디가 시작인지 먼저 찾기
-    for i in range(row):
-        for j in range(col):
-            if arr[i][j] == 'S':
-                sx, sy = i, j
-                
-    def bfs(x, y, end):
-        q= deque()
-        q.append([x,y])
-        visited = [[-1]*col for _ in range(row)]
-        visited[x][y] = 0
-        
-        while q:
-            x, y = q.popleft()
-            if arr[x][y] == end:
-                return [visited[x][y], x, y]
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if (0<= nx < row) and (0<= ny < col):
-                    if visited[nx][ny] == -1:
-                        if arr[nx][ny] != 'X': # 'O'인지 'L'인지 따질 필요 X
-                            q.append([nx, ny])
-                            visited[nx][ny] = visited[x][y]+1 #visited 표시
-        return None
-    
-    cnt = bfs(sx, sy, 'L')
-    if cnt == None:
-        return -1
-    answer = cnt[0]
-    cnt = bfs(cnt[1], cnt[2], 'E')
-    if cnt == None:
-        retrun -1
-    answer += cnt[0]          
-    return answer
-'''
-from collections import deque
-def solution(maps):
-    answer = 0
-    direction = [[0,1],[0,-1],[1,0],[-1,0]]
     n,m = len(maps),len(maps[0])
     # 출발 지점
     for i in range(n):
@@ -74,9 +31,9 @@ def solution(maps):
             x,y = q.popleft()
             if maps[x][y] == end:
                 return [visited[x][y],x,y]
-            for dir in direction:
-                nx = x + dir[0]
-                ny = y + dir[1]
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
                 if 0 <= nx < n and 0 <= ny < m:
                     if visited[nx][ny] == -1:
                         if maps[nx][ny] != 'X':
